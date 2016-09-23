ef7760bda45cf5bed53fea31c59626f731a90b30
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2016-09-22 21:26:18 -0700

    whatever commit

--- omnizonk.js
+++ omnizonk.js
@@ -5,18 +5,29 @@ MAX_EDO = 50;
 BASE_GAIN = 0.001;
 BASE_FREQ = 440;
 
-for(var i = 1; i <= MAX_EDO; i++) {
+for(var i = 0; i <= MAX_EDO; i++) {
   addEdo(i);
 }
 
 function addEdo(edo) {
   for(var j = 0; j < edo; j++) {
-    addVoice(edo, j);
+    // if (j/edo > .5) {
+    //   addVoice(edo, j, true);
+    // } else if (j/edo === .5) {
+    //   if (Math.random() > .5) {
+    //     addVoice(edo, j, true);
+    //   } else {
+    //     addVoice(edo, j);
+    //   }
+    // } else {
+      addVoice(edo, j);
+    // }
   }
 }
 
-function addVoice(edo, j) {
+function addVoice(edo, j, lowerByOctave) {
   var rounded = (j / edo).toPrecision(16);
+  if (lowerByOctave) rounded /= 2;
   if (voices[rounded]) {
     voices[rounded].count++;
     voices[rounded].gainNode.gain.value = BASE_GAIN * voices[rounded].count;
