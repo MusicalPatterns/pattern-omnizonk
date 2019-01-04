09d49adee6b81f1de0a8618bc1040c8f53661d62
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-04 12:37:50 -0800

    confirm snapshot test

--- dist/indexForTest.d.ts
+++ dist/indexForTest.d.ts
@@ -1,5 +1,6 @@
 export { buildEntities, buildScales, buildNoteSpec, } from './materials/indexForTest';
 export { buildEqualDivisions, } from './custom/indexForTest';
 export { OmnizonkPatternSpec, } from './types';
+export { pattern } from './patterns';
 import * as snapshot from './snapshot.json';
 export { snapshot };
--- dist/indexForTest.js
+++ dist/indexForTest.js
@@ -7,6 +7,8 @@ exports.buildScales = indexForTest_1.buildScales;
 exports.buildNoteSpec = indexForTest_1.buildNoteSpec;
 var indexForTest_2 = require("./custom/indexForTest");
 exports.buildEqualDivisions = indexForTest_2.buildEqualDivisions;
+var patterns_1 = require("./patterns");
+exports.pattern = patterns_1.pattern;
 // tslint:disable-next-line:no-default-import
 var snapshot = require("./snapshot.json");
 exports.snapshot = snapshot;
