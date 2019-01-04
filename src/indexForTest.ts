b128d032a6acb84495e54a3671334fa94d9ad153
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-04 09:35:00 -0800

    run snapshot test

--- src/indexForTest.ts
+++ src/indexForTest.ts
@@ -11,6 +11,7 @@ export {
 export {
     OmnizonkPatternSpec,
 } from './types'
+export { pattern } from './patterns'
 
 // tslint:disable-next-line:no-default-import
 import * as snapshot from './snapshot.json'
