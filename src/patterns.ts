675994c2e1dfb82e7f2e56af7e1f5427438c3f13
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-12 09:10:22 -0800

    1.0.29: alwaysdate

--- src/patterns.ts
+++ src/patterns.ts
@@ -20,7 +20,7 @@ const material: PatternMaterial = {
 const metadata: PatternMetadata = {
     description: post,
     formattedName: 'Omnizonk',
-    mostRecentPublish: process.env.PUBLISH_DATE,
+    mostRecentPublish: process.env.PUBLISH_DATE || '2016-09-10T07:00:00.000Z',
     musicalIdeaIllustrated: 'Farey sequence as a tone cluster',
     originalPublish: '2016-09-10T07:00:00.000Z',
 }
