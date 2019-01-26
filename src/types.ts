ea0decf0888ee8a0ba7b7eba3a5752ae38adfeac
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-25 16:00:05 -0800

    1.0.61: shouldextendspec

--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,13 @@
-import {
-    RangedSpecPropertyAttributes,
-    StandardSpec,
-    StandardSpecAttributes,
-} from '@musical-patterns/pattern'
+import { RangedSpecPropertyAttributes, Spec, SpecAttributes } from '@musical-patterns/pattern'
 import { Base, Denominator } from '@musical-patterns/utilities'
 
-interface OmnizonkSpec extends StandardSpec {
+interface OmnizonkSpec extends Spec {
     maxEqualDivision: Denominator,
     minEqualDivision: Denominator,
     window: Base,
 }
 
-interface OmnizonkSpecAttributes extends StandardSpecAttributes {
+interface OmnizonkSpecAttributes extends SpecAttributes {
     maxEqualDivision: RangedSpecPropertyAttributes,
     minEqualDivision: RangedSpecPropertyAttributes,
     window: RangedSpecPropertyAttributes,
