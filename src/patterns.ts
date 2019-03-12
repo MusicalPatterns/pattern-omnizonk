ad27aefa1273ed9858964f13c83a9870680851ba
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-03-11 22:13:39 -0700

    1.0.106: metadata and material should live in their modules, not in pattern

--- src/patterns.ts
+++ src/patterns.ts
@@ -1,22 +1,7 @@
-import { Material } from '@musical-patterns/compiler'
-import { Id, Metadata, Pattern, Patterns } from '@musical-patterns/pattern'
-import { materializeEntities, materializeScales } from './material'
-import { post } from './metadata'
+import { Id, Pattern, Patterns } from '@musical-patterns/pattern'
+import { material } from './material'
+import { metadata } from './metadata'
 import { OmnizonkSpecs, spec } from './spec'
-
-const material: Material = {
-    materializeEntities,
-    materializeScales,
-}
-
-const metadata: Metadata = {
-    description: post,
-    formattedName: 'Omnizonk',
-    mostRecentPublish: process.env.PUBLISH_DATE || '2016-09-10T07:00:00.000Z',
-    musicalIdeaIllustrated: 'Farey sequence as a tone cluster',
-    originalPublish: '2016-09-10T07:00:00.000Z',
-    version: process.env.PATTERN_VERSION || 'unknown',
-}
 
 const pattern: Pattern<OmnizonkSpecs> = {
     id: Id.OMNIZONK,
