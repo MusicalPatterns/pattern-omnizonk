41ebf73425eff32fba2dbe599d5f47d3db81c937
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-03-29 20:07:10 -0700

    1.0.116: exclude snapshot from package

--- src/indexForTest.ts
+++ src/indexForTest.ts
@@ -14,8 +14,3 @@ export {
     OmnizonkSpecs,
 } from './spec/indexForTest'
 export { pattern } from './patterns'
-
-// tslint:disable-next-line no-default-import
-import * as snapshot from './snapshot.json'
-
-export { snapshot }
